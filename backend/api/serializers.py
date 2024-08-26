007955582a1fcdb556ebad8a77daf01f09ca4f4c
ctake <ctake0099@gmail.com>
2024-08-26 16:40:37 +0900

    create api

---
+++ backend/api/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import User, Book, UserBook
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'created_at']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        # パスワードのハッシュ化を行う
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+        )
+        return user
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'author', 'caption', 'publication_date', 'isbn', 'created_at']
+
+class UserBookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserBook
+        fields = ['id', 'user', 'book', 'book_memo', 'added_at']
+        read_only_fields = ['user', 'book', 'added_at']
+
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Book, UserBook
+from .serializers import UserSerializer, BookSerializer, UserBookSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class UserBookViewSet(viewsets.ModelViewSet):
+    queryset = UserBook.objects.all()
+    serializer_class = UserBookSerializer
